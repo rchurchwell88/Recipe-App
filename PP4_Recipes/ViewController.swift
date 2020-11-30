cb9bab40d23b3fbb68151f14bb564418911e2788
Russell Churchwell <russellchurchwell@russells-mbp.attlocal.net>
2020-11-29 21:33:49 -0600

    Firebase is now implemented/supported but not utilized.

--- PP4_Recipes/ViewController.swift
+++ PP4_Recipes/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testPushUser()
     }
-
+    
+    func testPushUser(){
+        Auth.auth().createUser(withEmail: "worked@worked.com", password: "password", completion: nil)
+    }
 
 }
 
---
+++ Pods/Target Support Files/Pods-PP4_Recipes/Pods-PP4_Recipes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PP4_Recipes : NSObject
+@end
+@implementation PodsDummy_Pods_PP4_Recipes
+@end
