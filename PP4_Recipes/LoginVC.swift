abf82b41b3154db55aa9ff1f8f0ec1235be4ed30
Russell Churchwell <russellchurchwell@russells-mbp.attlocal.net>
2020-12-04 00:06:08 -0600

    Registration 90% done. Pre alpha pushes.

---
+++ PP4_Recipes/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  PP4_Recipes
+//
+//  Created by Russell Churchwell on 12/2/20.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
